12858affac03423aee9c470b9a0e95b9e570911e
priyanka-swiftrut <priyanka.dudhat@swiftrut.com>
2025-01-20 13:43:10 +0530

    add user login functionality with validation

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -26,3 +26,24 @@ module.exports.registerUser = async (req, res, next) => {
     next(error);
   }
 };
+
+module.exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const isPasswordMatch = await user.comparePassword(password);
+  if (!isPasswordMatch) {
+    return res.status(401).json({ message: "Invalid password" });
+  }
+
+  const token = user.generateToken();
+  res.status(200).json({ token, user });
+}
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -10,6 +10,10 @@ router.post('/register',[
     body('fullName.lastName').isLength({min:2}).withMessage('Last name must be at least 2 characters long'),
 ],userController.registerUser);
 
+router.post('/login',[
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('password').isLength({min:6}).withMessage('Password must be at least 6 characters long'),
+],userController.loginUser);
 
 
 module.exports=router;
